f41a031db1a573a28c5666ff4137bcf7040f98c9
anayatrabbi86@gmail.com <anayatrabbi86@gmail.com>
2021-04-23 14:54:19 +0600

    port updated and nodemone fixed

--- index.js
+++ index.js
@@ -88,6 +88,4 @@ app.delete('/deleteRegistration/:id', (req, res) => {
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
